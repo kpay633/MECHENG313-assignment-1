8796ee20bc375a1859b18b643e2e866f56a59d90
kpayinda <kpayinda@gmail.com>
2025-04-14 22:41:43 +1200

    section 2.1 is working perfectly ? now

--- 2.1/2.1.ino
+++ 2.1/2.1.ino
@@ -2,20 +2,21 @@
 #include <avr/io.h>
 #include <inttypes.h>
 
-#define THRESHOLD 4
+#define THRESHOLD 3
+#define TH_V THRESHOLD*1024/5
 
-volatile uint8_t adc_result;
+volatile uint16_t adc_result;
 
 int main(void) {
   //setup:
   cli();
-  DDRB |= (1 << DDB3); //set PORTB3 as an output pin.
   DDRB |= (1 << DDB5);//onboard LED PB5 output. 
+  DDRC &= ~(1 << DDC1); //set A1 as input (arduino pc1)
 
   Serial.begin(9600);
 
   ADMUX |= (1 << MUX0); //using ADC1
-  ADMUX |= (1 << REFS0); //sets Vref to Vcc 5V (internal).
+  ADMUX &= ~((1 << REFS0)|(1 << REFS1)); //AREF
   ADCSRA |= (1 << ADEN)|(1 << ADIE); //adc enable and int enable.
   ADCSRA |= (1 << ADPS2)|(1 << ADPS1)|(1 << ADPS0); //division factor 128
   //ADCSRB |= (1 << ADTS2)|(1 << ADTS0); //for TC1, this is setting trigger source
@@ -25,8 +26,9 @@ int main(void) {
   DDRC &= ~(1 << DDC1); //set pin A1 as input. (later read PINC1)
   TCCR1B |= (1 << WGM12); //timer1 in CTC mode. 
   TCCR1B |= (1 << CS12)|(1 << CS10); //timer1 prescaler 1024.
-  TIMSK0 |= (1 << OCIE0A); //output compare match A int enable.
-  OCR1A = 15625; //timer1 reaches this CV at 1000ms hence OF int. 
+  OCR1A = 15625; //timer1 raches this CV at 1000ms hence OF int.
+
+  TIMSK1 |= (1 << OCIE1A); //output compare match A int enable.
   sei();
 
   TCNT1 = 0;
@@ -34,13 +36,13 @@ int main(void) {
 
   while(1) {
 
-      if (adc_result >= THRESHOLD) {
+      if (adc_result >= TH_V) {
         if (TCNT1 == 3906){PORTB |= (1 << PORTB5);}
         if (TCNT1 == 7813){PORTB &= ~(1 << PORTB5);}
         if (TCNT1 == 11719){PORTB |= (1 << PORTB5);}
         if (TCNT1 == 15624){PORTB &= ~(1 << PORTB5);}        
       }
-      if (adc_result < THRESHOLD) {
+      if (adc_result < TH_V) {
         if (TCNT1 == 3906){PORTB |= (1 << PORTB5);}
         if (TCNT1 == 7813){PORTB &= ~(1 << PORTB5);}       
       }
@@ -51,8 +53,10 @@ int main(void) {
 ISR(TIMER1_COMPA_vect) {
   //this occurs every sec. read ADC here.
   ADCSRA |= (1 << ADSC); //this is to start a conversion. idk if we want it here. 
+  //Serial.print("balls\n");
 }
 
 ISR(ADC_vect) {
   adc_result = ADC;
+  Serial.print(adc_result);
 }
